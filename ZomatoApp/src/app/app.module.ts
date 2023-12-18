f8a1ff9f5a06d29332ae90de6a239025f4b33d8d
Ajit Satpute <ajit.git174@gmail.com>
2023-12-18 11:42:20 +0530

    List-Card-Created

--- ZomatoApp/src/app/app.module.ts
+++ ZomatoApp/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CityListComponent } from './city-list/city-list.component';
+import { ListCardComponent } from './city-list/list-card/list-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CityListComponent,
+    ListCardComponent
   ],
   imports: [
     BrowserModule,
