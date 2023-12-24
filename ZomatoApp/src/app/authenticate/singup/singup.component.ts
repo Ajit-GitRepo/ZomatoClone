2d896a9ace5016692cb49ba37cf1bc504caa3467
Ajit Satpute <ajit.git174@gmail.com>
2023-12-24 08:48:06 +0530

    pages added : log-in, sing-up,updated footer position and updated styling of all mentioned component

--- ZomatoApp/src/app/authenticate/singup/singup.component.ts
+++ ZomatoApp/src/app/authenticate/singup/singup.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+
+interface SingUpData{
+  username:string;
+  email:string;
+  agree:boolean;
+}
+
 
 @Component({
   selector: 'app-singup',
@@ -7,4 +16,22 @@ import { Component } from '@angular/core';
 })
 export class SingupComponent {
 
+  userinput :SingUpData
+  isAgree:boolean=false;
+  
+  constructor(){
+    this.userinput={username:"Amruta",email:'Amruta@gmail.com',agree:true};
+    // console.log(this.agree);
+                
+  }
+
+ 
+  OnSingup(Data:NgForm){
+
+      console.log(Data.value);
+  }
+
+  agree(){
+    this.isAgree !=this.isAgree ;
+  }
 }
